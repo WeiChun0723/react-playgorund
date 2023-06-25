ee80bc5fcf27732c6ccbcc903dd6385a6d0eb15a
WeiChun0723 <laiwc0723@gmail.com>
2023-06-25 15:19:59 +0800

    Implement local storage to weatherforecast list to reduce api call

--- src/component/weatherApp/WeatherForecastList.tsx
+++ src/component/weatherApp/WeatherForecastList.tsx
@@ -1,20 +1,34 @@
 import axios, { AxiosError } from "axios";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { WeatherResponse } from "./WeatherType";
 
-
 function WeatherForecastList() {
+  console.log('Component rendered');
   const foreCastURL =
     "https://api.weatherbit.io/v2.0/forecast/daily?postal_code=52000&country=MY&days=7&key=58e07d95820046e689e7410c74f5e526";
 
-  const [response, setResponse] = useState<WeatherResponse | null>(null);
+  const [response, setResponse] = useState<WeatherResponse | null>(() => {
+    const localValue = localStorage.getItem("WeatherResponse");
+    if (localValue === null) return null;
+
+    return JSON.parse(localValue) as WeatherResponse;
+  });
   const [error, setError] = useState<AxiosError | null>(null);
 
   useEffect(() => {
-    axios
-      .get(foreCastURL)
-      .then((response) => setResponse(response.data as WeatherResponse))
-      .catch((error) => setError(error as AxiosError));
+    if (response === null) {
+      axios
+        .get(foreCastURL)
+        .then((response) => {
+          console.log("Get Response from api");
+          setResponse(response.data as WeatherResponse);
+          localStorage.setItem(
+            "WeatherResponse",
+            JSON.stringify(response.data)
+          );
+        })
+        .catch((error) => setError(error as AxiosError));
+    }
   }, []);
 
   if (error) {
@@ -23,16 +37,27 @@ function WeatherForecastList() {
     return (
       <div className="row row-cols-1 row-cols-md-3 g-4">
         {response.data.map((data) => (
-          <div className="col">
-            <div className="card h-100">
-              <img
-                src={`https://cdn.weatherbit.io/static/img/icons/${data.weather.icon}.png`}
-                className="card-img-top"
-                alt="..."
-              />
-              <div className="card-body">
-                <h5 className="card-title">{data.datetime}</h5>
-                <p className="card-text">{data.weather.description}</p>
+          <div className="col" key={data.datetime}>
+            <div className="card mb-3" style={{ maxWidth: "540px" }}>
+              <div className="row g-0">
+                <div className="col-md-4">
+                  <img
+                    src={`https://cdn.weatherbit.io/static/img/icons/${data.weather.icon}.png`}
+                    className="img-fluid rounded-start"
+                    alt="..."
+                  />
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <h5 className="card-title">{data.datetime}</h5>
+                    <p className="card-text">{data.weather.description}</p>
+                    <p className="card-text">
+                      <small className="text-body-secondary">
+                        {data.max_temp}°C
+                      </small>
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -40,7 +65,11 @@ function WeatherForecastList() {
       </div>
     );
   } else {
-    return <div className="col text-center"><img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif"></img></div>;
+    return (
+      <div className="col text-center">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif"></img>
+      </div>
+    );
   }
 }
 
